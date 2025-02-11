bda8495e2e48e6f59ed4fb5de6a002a412ebaaa2
Ludvig Alvir <ludvi@OSL_Ludvig_FED1.res.bitpro.as>
2025-02-11 09:48:06 +0100

    plain version

--- module-3/react-routing/src/App.jsx
+++ module-3/react-routing/src/App.jsx
@@ -2,21 +2,42 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [route, setRoute] = useState("");
-  const path = window.location.pathname;
-  console.log(path);
+  const [route, setRoute] = useState(window.location.pathname);
 
   return (
     <>
       <header>
-        <a href="/" className="text-blue-500 mx-4">
+        <a
+          href="/"
+          className="text-blue-500 mx-4"
+          onClick={(e) => {
+            e.preventDefault();
+
+            setRoute("/");
+          }}
+        >
           Home
         </a>
-        <a href="/about" className="text-blue-500 mx-4">
+        <a
+          href="/about"
+          className="text-blue-500 mx-4"
+          onClick={(e) => {
+            e.preventDefault();
+
+            setRoute("/about");
+          }}
+        >
           About
         </a>
       </header>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+
+      {route == "/" ? <h1 className="text-3xl font-bold">Home page</h1> : ""}
+
+      {route == "/about" ? (
+        <h1 className="text-3xl font-bold">About page</h1>
+      ) : (
+        ""
+      )}
     </>
   );
 }
