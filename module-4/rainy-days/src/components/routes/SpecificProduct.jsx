9e13e3cfde2a6e6c322a372ef952b931e8bc7674
Ludvig Alvir <ludvi@OSL_Ludvig_FED1.res.bitpro.as>
2025-02-19 12:06:42 +0100

    lecture

--- module-4/rainy-days/src/components/routes/SpecificProduct.jsx
+++ module-4/rainy-days/src/components/routes/SpecificProduct.jsx
@@ -1,7 +1,48 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 export function SpecificProduct() {
-  const params = useParams();
-  console.log(params);
+  const { id } = useParams();
+  console.log(id);
+  const [product, setProduct] = useState(false);
 
-  return <h1>Specific</h1>;
+  const url = "https://v2.api.noroff.dev/rainy-days/";
+
+  console.log(url + id);
+
+  useEffect(() => {
+    async function getProduct() {
+      const res = await fetch(url + id);
+      const data = await res.json();
+      console.log(data);
+      setProduct(data.data);
+    }
+    getProduct();
+  }, []);
+
+  return (
+    <>
+      <h1>Specific</h1>
+      {product ? (
+        <div>
+          <img
+            src={product.image.url}
+            alt={product.image.alt}
+            className="h-64"
+          />
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          {product.onSale ? (
+            <>
+              <p className="line-through">{product.price}</p>
+              <p>{product.discountedPrice}</p>
+            </>
+          ) : (
+            <p>{product.price}</p>
+          )}
+        </div>
+      ) : (
+        <p>loading</p>
+      )}
+    </>
+  );
 }
